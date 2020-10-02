201c2bc14323e5f2c67732403b6779b38f881e02
gaurav <gaurav.ag45@gmail.com>
2020-10-02 16:29:13 +0530

    Test Class Hello print

--- TestClass.java
+++ TestClass.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Commit and push");
+		
+		Scanner input = new Scanner(System.in);
+		
+		int num = input.nextInt();
+		
+		if(num == 1) {
+			System.out.println("hello 1");
+		}else {
+			System.out.println("default");
+		}
 
 	}
 
