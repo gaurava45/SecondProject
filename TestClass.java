8d0fd8cc6fca72dbdbd56af20f79a8f835dd1e3c
gaurava45 <70994685+gaurava45@users.noreply.github.com>
2020-10-02 16:31:20 +0530

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -12,6 +12,8 @@ public class TestClass {
 		
 		if(num == 1) {
 			System.out.println("hello 1");
+		}else if(num == 2){
+			System.out.println("hello 2");
 		}else {
 			System.out.println("default");
 		}
